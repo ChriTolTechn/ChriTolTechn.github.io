d1a3af8948577889d6545e48c51c151c8d6fb16b
Chris <if22b198@technikum-wien.at>
2022-12-26 15:46:39 +0100

    First button is working :o

---
+++ main.js
@@ -0,0 +1,20 @@
+import { ethers } from "./node_modules/ethers/dist/ethers.esm.js";
+const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
+const rpcProvider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/zUOydiqRKAPMR39RIqcefp6DfpFrP0k-");
+
+(async () => {
+    await main();
+})();
+
+async function main(){
+    await metamaskProvider.send("eth_requestAccounts", []);
+    const metamaskSigner = metamaskProvider.getSigner()
+}
+
+async function getCurrentBlockNumber(){
+    return await rpcProvider.getBlockNumber()
+}
+
+document.getElementById("currentBlockButton").onclick = async function() {
+    document.getElementById("currentBlockText").innerHTML = await getCurrentBlockNumber();
+};
