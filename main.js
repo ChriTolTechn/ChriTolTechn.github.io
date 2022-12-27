a6ada9f8bf8546b539a383aa47f6518a4d37acdf
Chris <if22b198@technikum-wien.at>
2022-12-27 11:38:43 +0100

    added third feature

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { ethers } from '../modules/ethers/dist/ethers.esm.min.js';
 ////////////////////////////////////////
 // Constants
 const tomatoTokenAddress = "0x78b883811F0E7c5551A52cC8DCD6CF37e30e2756";
+const recipientAddress = "0x15433DA387451F9dE4565280C85506CB71aF9376";
 const abi = [
     // Read-Only Functions
     "function balanceOf(address owner) view returns (uint256)",
@@ -27,6 +28,7 @@ var signer;
 try {
     metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
 } catch(e){
+    console.log(e);
     document.getElementById("metaMaskStatus").innerHTML = "Status: Metamask not available";
     document.getElementById("metaMaskStatus").style.color = "yellow";
 }
@@ -42,8 +44,10 @@ async function connectToMetamask(){
 
     try {
         await metamaskProvider.send("eth_requestAccounts", []); 
+        signer = metamaskProvider.getSigner();
+        erc20 = new ethers.Contract(tomatoTokenAddress, abi, signer);
     } catch(e){
-
+        console.log(e);
     }
     await checkConnection();
 }
@@ -52,9 +56,9 @@ async function checkConnection() {
     if(metamaskProvider == null) return;
 
     try{
-        signer = metamaskProvider.getSigner();
         console.log("Account:", await signer.getAddress());
     } catch(e) {
+        console.log(e);
         document.getElementById("metaMaskStatus").innerHTML = "Status: Not Connected";
         document.getElementById("metaMaskStatus").style.color = "red";
         return;
@@ -63,31 +67,42 @@ async function checkConnection() {
     document.getElementById("metaMaskStatus").style.color = "green";
 }
 
+// Exercise 6, 1
 async function getCurrentBlockNumber(){
     if(metamaskProvider == null) return;
 
     return await metamaskProvider.getBlockNumber()
 }
 
+// Exercise 6, 2
 async function getCurrentTomatoTokenBalance(){
     if(metamaskProvider == null) return;
-    
     if(signer == null) return;
-
-    if(erc20 == null){
-        try {
-            erc20 = new ethers.Contract(tomatoTokenAddress, abi, signer);
-        } catch(e) {
-            // TODO error handling
-            return -1;
-        }
-    }
+    if(erc20 == null) return;
 
     try {
         return await erc20.balanceOf(signer.getAddress())
     } catch(e) {
-        // TODO error handling
+        console.log(e);
         return -1;
+    }
+}
+
+// Exercise 6, 3
+async function transferToken(){
+    if(metamaskProvider == null) return;
+    if(signer == null) return;
+    if(erc20 == null) return;
+
+    try {
+        ethers.utils.formatUnits(await erc20.balanceOf(signer.getAddress()));
+        document.getElementById("sendToAccountText").innerHTML = "Waiting for confirmation";
+        var tx = await erc20.transfer(recipientAddress, ethers.utils.parseUnits("10"));
+        await tx.wait();
+        return true;
+    } catch(e) {
+        console.log(e);
+        return false;
     }
 }
 
@@ -112,5 +127,16 @@ document.getElementById("tomatoTokenBalanceButton").onclick = async function() {
     var balance = await getCurrentTomatoTokenBalance();
     if(balance >= 0){
         document.getElementById("currentTomatoTokenText").innerHTML = ethers.utils.formatEther(balance) + " TMT";
+    }
+};
+
+document.getElementById("sendToAccountButton").onclick = async function() {
+    if(metamaskProvider == null) return;
+
+    var success = await transferToken();
+    if(success) {
+        document.getElementById("sendToAccountText").innerHTML = "Success";
+    } else {
+        document.getElementById("sendToAccountText").innerHTML = "Failed";
     }
 };
